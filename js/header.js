8ff15592b2ab3f0dbcece499676466ed7718d771
NoritoMinagoshi <60051907+NoritoMinagoshi@users.noreply.github.com>
2020-04-07 09:39:49 +0900

    Delete header.js

--- js/header.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.header-icon').on('click', function() {
-    if($('.header-nav ul').css('display') === 'block') {
-      $('.header-nav ul').slideUp('1500');
-    } else {
-      $('.header-nav ul').slideDown('1500');
-    }
-  });
-});
